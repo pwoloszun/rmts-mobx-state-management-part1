5a77757da44d9101ed497a24fe515f7c0c1678fd
Piotr Woloszun <piotr.woloszun@gmail.com>
2021-03-12 07:22:19 +0100

    v0.1.1

--- src/application/projects-management/ProjectsPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function AccountSettingsPage(): React.ReactElement {
-  return (
-    <div>
-      AccountSettingsPage
-    </div>
-  );
-}
--- src/domain/todos-coupled/store/todos.store.ts
+++
@@ -1,6 +0,0 @@
-import Todo from './todo.model';
-import TodoList from './todo-list.model';
-
-const todoStore = new TodoList([new Todo("Get Coffee"), new Todo("Write simpler code")]);
-
-export default todoStore;
